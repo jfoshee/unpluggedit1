d7c4225a4260537078f6521a4704ace5098c8a2f
unknown <jfoshee@.(none)>
2011-04-15 18:21:21 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unpluggedit1::Application.config.secret_token = '8d2e8391d0fc770faee806976c164047bda90003a727cfa8d540583958d7cc18fada989096535c3252deb7fd6a1d07905ec49a39138e2ba79e68520ba1db7de3'
