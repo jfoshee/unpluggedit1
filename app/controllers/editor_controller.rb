d943bca1832db1875c0f259f3b661afa5b95fa44
Jacob Foshee <jacobf@gmail.com>
2011-04-19 00:27:10 -0500

    Solved the 'only works once' problem by nuking the working directory.  Have no idea if this sort of stuff will work on Heroku...

--- app/controllers/editor_controller.rb
+++ app/controllers/editor_controller.rb
@@ -3,11 +3,15 @@ class EditorController < ApplicationController
 #    working_dir = 'C:\\Users\\jfoshee\\Documents\\My Dropbox\\Rails\\dummy'
 #    @g = Git.open(working_dir) 
 
+    working_dir = 'tmp/dummy'
+    FileUtils::rm_rf(working_dir)
+    
     # uri = 'https://jfoshee@github.com/jfoshee/dummy.git'
     uri = 'git://github.com/jfoshee/dummy.git'
-    @g = Git.clone(uri, 'tmp/dummy')
+    @g = Git.clone(uri, working_dir)
 
-    lines = File.open('tmp/dummy/app/views/dummy/index.html.erb').readlines
+    @file_path = '/app/views/dummy/index.html.erb'
+    lines = File.open(working_dir + @file_path).readlines
     @text = ""
     lines.each do |line|
       @text += line
